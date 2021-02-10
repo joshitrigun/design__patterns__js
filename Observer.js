bc462a34b0165196a0de764ce75448264bc39e4c
Trigun Joshi <trigunjoshi@gmail.com>
2021-02-09 23:08:06 -0800

    Add files via upload

---
+++ Observer.js
@@ -0,0 +1,36 @@
+
+
+function Subject () {
+	this.observers = [];
+	
+}
+Subject.prototype = {
+	
+	subscribe: function(fn){
+		this.observers.push(fn)
+	},
+	unsubscribe: function(fnToRemove){
+		
+		this.observers = this.observers.filter( fn => {
+			if(fn != fnToRemove)
+			return fn
+		})
+	},
+	fire: function()
+	{
+		this.observers.forEach(fn => {fn.call()})
+	}
+} 
+const subject = new subject()
+function Observer1(){
+	console.log('Ob1 firing');
+}
+function Observer2(){
+	console.log('Ob2 firing');
+}
+subject.subscribe(Observer1)
+subject.subscribe(Observer2)
+subject.fire()
+
+subject.unsubscribe(Observer1)
+subject.fire()
